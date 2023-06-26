6ddd268ffbef230539b58a8fb7a316ab548dfa59
Cynthiadwi <112931296+Cynthiadwi@users.noreply.github.com>
2023-06-27 03:42:14 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,159 @@
+  <?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	    <!-- Css Styles -->
+          <meta charset="UTF-8">
+    <meta name="description" content="Male_Fashion Template">
+    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Page</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="  text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">   
+    
+</head>
+<body>
+<?php
+include("header.php");
+?>
+<section id="aboutUs">
+  <div class="container">
+    <div class="heading text-left"> 
+      <!-- Heading -->
+      <div>
+      	<h2><u>About Us</u></h2>
+      </div>
+      <div class="wel_line"><center><h3>WELCOME TO TIXIE THEATER  </h3></center></div>
+    </div>
+    <div class="row feature design">
+
+      <div class="area1 columns left">
+        
+      <h5>TIXIE Theater adalah merek pameran film yang paling dikenal di negara ini, yang memiliki standar tinggi dalam 'Kemewahan', 'Pelayanan', dan 'Teknologi'.</h5>
+    <p>Saat ini dengan jaringan bioskop sebanyak 450+ layar yang tersebar di lebih dari 100 properti yang mencakup 100+ kota di seluruh negeri.
+    Hal terbaik tentang penggunaannya adalah kesederhanaan dan kemudahan dengan mana seseorang dapat memesan tiket secara online. Berasal dari Semarang, yang memiliki warisan sinema yang kaya, kami adalah jaringan teater dengan semangat untuk pengalaman dan semangat yang lebih besar untuk berbagi dengan pelanggan kami. Kami memahami kegembiraan menonton cerita favorit kami menjadi hidup di layar besar, dan kami percaya bahwa tugas kami adalah pergi lebih jauh untuk membuat pengalaman ini semakin mendalam sebisa mungkin!
+
+    <br>
+Tersedia selama 24 jam sehari, 7 hari seminggu.</p>
+
+      </div>
+      <div class="area2 columns feature-media left"> <img src="image/Online-Movie-Ticket-Booking-Banner-1-1280x720.jpg" alt="" width="100%"> </div>
+    </div>
+  
+    </div>
+  
+</section>
+<div class="dvCapacity">
+
+                                    <div class="CapacityHead">
+                                        SERVING HAPPINESS
+                       
+                       
+                                    </div>
+                                    <div class="CapHorizon">
+                                    </div>
+                                    <div class="PatronStruc">
+                                        <div class="row">
+
+
+
+                                            <div class="col-md-4">
+
+                                                <div class="dvPatron">
+                                                    <img src="image/unnamed.jpg" class="resize" alt="#" />
+
+                                                </div>
+
+                                                <div class="dvPatConetnt">
+                                                    <div class="dvPatronDesc">
+                                                        <div class="dvPatronDescHead">
+                                                            Luxury
+
+                                                        </div>
+                                                        <div class="dvPatronDescText">
+                                                           TIXIE Theater adalah merek pameran film yang paling dikenal di negara ini, yang memiliki standar tinggi dalam 'Kemewahan', 'Pelayanan', dan 'Teknologi'.
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                            <div class="col-md-4">
+
+                                                <div class="dvPatron">
+                                                    <img src="image/food_epicurious1-1-1d876222fa3d4f92.jpg" alt="#" />
+
+                                                </div>
+                                                <div class="dvPatConetnt">
+                                                    <div class="dvPatronDesc">
+                                                        <div class="dvPatronDescHead">
+                                                            Service
+                                                        </div>
+                                                        <div class="dvPatronDescText">
+                                                         Weekend atau weekday kami selalu siap melayani anda dengan baik
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+                                            <div class="col-md-4">
+
+                                                <div class="dvPatron">
+                                                    <img src="image/capacity-technology.jpg" class="resize" alt="#" />
+
+                                                </div>
+                                                <div class="dvPatConetnt">
+                                                    <div class="dvPatronDesc">
+                                                        <div class="dvPatronDescHead">
+                                                            Technology
+                                                        </div>
+                                                        <div class="dvPatronDescText">
+                                                            Baik itu proyeksi maupun sound system kami selalu mengikuti perkembangan teknologi terbaru
+
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+<?php
+include("footer.php");
+?>
+
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>     
